3428270c8d41cce7221513abffb74eca1db32b63
Dror Guzman <dror@Drors-MacBook-Pro.local>
2025-01-26 14:00:31 -0500

    added backtrack with for loop

---
+++ src/backtrack_permutations.py
@@ -0,0 +1,37 @@
+ 
+
+class Solution(object):
+    def solve(self, my_list):
+        
+        res = []
+        combo = []
+        def backtrack(i, combo):
+            if i>=len(my_list):
+                return
+            if len(combo)==len(my_list):
+                res.append(combo[:])
+                return
+            
+
+            for ix in range(len(my_list)):
+                if not my_list[ix] in combo:
+                    combo.append(my_list[ix])
+                    backtrack(ix,combo)
+                    combo.pop() 
+
+        backtrack(0,combo)
+        return res
+        
+
+
+if __name__=='__main__':
+    print ("start...")
+    
+    s = Solution()
+
+    my_list = [1,2,3,4]   
+    res = s.solve(my_list)
+    for r in res:
+        print (r)
+    
+    print ("END")
